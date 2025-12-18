02cf2e3d0e00ccb63a0a4030a0cdcda8945808ca
NguyenTuKien <KienNT.B23CN465@stu.ptit.edu.vn>
2025-12-18 21:54:00 +0700

    Update tool

--- Codeptit/Tools/ZipLinux.java
+++ Codeptit/Tools/ZipLinux.java
@@ -7,9 +7,6 @@ public class ZipLinux {
 
     static Scanner cin = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
-        if (!cin.hasNext()) {
-            System.out.println("Hãy nhập tên folder bạn cần zip");
-        }
         String folder = cin.next();
         String zipFile = folder + "/" + folder + ".zip";
         ProcessBuilder pb = new ProcessBuilder("zip", "-r", zipFile, folder);
--- Codeptit/Tools/ZipWindow.java
+++ Codeptit/Tools/ZipWindow.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
 public class ZipWindow {
     static Scanner cin = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
-        if (!cin.hasNext()) {
-            System.out.println("Hãy nhập tên folder bạn cần zip");
-        }
         String folder = cin.next();
         String zipFile = folder + "/" + folder + ".zip";
         ProcessBuilder pb = new ProcessBuilder("tar", "-acf", zipFile, folder);
